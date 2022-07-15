b7f02540f31f6eaeafed06d1bf18a2de71d255d3
Camila Morales <camila.moralesg21@gmail.com>
2022-07-15 00:37:44 -0500

    ADD: documentacion Swagger Se agrega la correspondiente documentacion a EquipoRouter

--- src/main/java/com/sofka/ejercicio/routers/EquipoRouter.java
+++ src/main/java/com/sofka/ejercicio/routers/EquipoRouter.java
@@ -2,6 +2,13 @@ package com.sofka.ejercicio.routers;
 
 import com.sofka.ejercicio.models.EquipoDTO;
 import com.sofka.ejercicio.usecases.equipousecase.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springdoc.core.annotations.RouterOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
@@ -20,6 +27,13 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 @Configuration
 public class EquipoRouter {
 
+    @RouterOperation(beanClass = CrearEquipoUseCase.class, beanMethod = "apply",
+            operation = @Operation(operationId = "crearEquipo", summary = "Crear equipo", tags = {"Equipo"},
+                    responses = {@ApiResponse(responseCode = "200", description = "Created"),
+                            @ApiResponse(responseCode = "401", description = "Unauthorized", content = { @Content(examples = { @ExampleObject(value = "")})}),
+                            @ApiResponse(responseCode = "403", description = "Forbidden", content = { @Content(examples = { @ExampleObject(value = "")})}),
+                            @ApiResponse(responseCode = "404", description = "Not found", content = { @Content(examples = { @ExampleObject(value = "")})}),
+                            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = { @Content(examples = { @ExampleObject(value = "")})}),}))
     @Bean
     public RouterFunction<ServerResponse> crearEquipo(CrearEquipoUseCase useCase){
         Function<EquipoDTO, Mono<ServerResponse>> executor = equipoDTO -> useCase.apply(equipoDTO)
@@ -33,6 +47,14 @@ public class EquipoRouter {
         );
     }
 
+    @RouterOperation(beanClass = ActualizarEquipoUseCase.class, beanMethod = "apply",
+            operation = @Operation(operationId = "actualizarEquipo", summary = "Actualizar equipo", tags = {"Equipo"},
+                    parameters = {@Parameter(in = ParameterIn.PATH, name = "id", description = "Integer")},
+                    responses = {@ApiResponse(responseCode = "200", description = "OK"),
+                            @ApiResponse(responseCode = "401", description = "Unauthorized", content = { @Content(examples = { @ExampleObject(value = "")})}),
+                            @ApiResponse(responseCode = "403", description = "Forbidden", content = { @Content(examples = { @ExampleObject(value = "")})}),
+                            @ApiResponse(responseCode = "404", description = "Not found", content = { @Content(examples = { @ExampleObject(value = "")})}),
+                            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = { @Content(examples = { @ExampleObject(value = "")})}),}))
     @Bean
     public RouterFunction<ServerResponse> actualizarEquipo(ActualizarEquipoUseCase useCase){
         Function<EquipoDTO, Mono<ServerResponse>> executor = equipoDTO -> useCase.apply(equipoDTO)
@@ -45,6 +67,14 @@ public class EquipoRouter {
                 request -> request.bodyToMono(EquipoDTO.class).flatMap(executor)
         );
     }
+    @RouterOperation(beanClass = EliminarEquipoPorIdUseCase.class, beanMethod = "apply",
+            operation = @Operation(operationId = "eliminarEquipo", summary = "Eliminar equipo", tags = {"Equipo"},
+                    parameters = {@Parameter(in = ParameterIn.PATH, name = "id", description = "Integer")},
+                    responses = {@ApiResponse(responseCode = "202", description = "Accepted"),
+                            @ApiResponse(responseCode = "401", description = "Unauthorized", content = { @Content(examples = { @ExampleObject(value = "")})}),
+                            @ApiResponse(responseCode = "403", description = "Forbidden", content = { @Content(examples = { @ExampleObject(value = "")})}),
+                            @ApiResponse(responseCode = "404", description = "Not found", content = { @Content(examples = { @ExampleObject(value = "")})}),
+                            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = { @Content(examples = { @ExampleObject(value = "")})}),}))
     @Bean
     public RouterFunction<ServerResponse> eliminarEquipoPorId(EliminarEquipoPorIdUseCase useCase){
         return route(
@@ -54,6 +84,14 @@ public class EquipoRouter {
                         .body(BodyInserters.fromPublisher(useCase.apply(request.pathVariable("id")), void.class))
         );
     }
+
+    @RouterOperation(beanClass = ObtenerEquiposUseCase.class, beanMethod = "get",
+            operation = @Operation(operationId = "obtenerEquipos", summary = "Obtener equipos", tags = {"Equipo"},
+                    responses = {@ApiResponse(responseCode = "200", description = "OK"),
+                            @ApiResponse(responseCode = "401", description = "Unauthorized", content = { @Content(examples = { @ExampleObject(value = "")})}),
+                            @ApiResponse(responseCode = "403", description = "Forbidden", content = { @Content(examples = { @ExampleObject(value = "")})}),
+                            @ApiResponse(responseCode = "404", description = "Not found", content = { @Content(examples = { @ExampleObject(value = "")})}),
+                            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = { @Content(examples = { @ExampleObject(value = "")})}),}))
     @Bean
     public RouterFunction<ServerResponse> obtenerEquipos(ObtenerEquiposUseCase useCase){
         return route(GET("/obtenerequipos").and(accept(MediaType.APPLICATION_JSON)),
@@ -62,6 +100,14 @@ public class EquipoRouter {
                         .body(BodyInserters.fromPublisher(useCase.get(), EquipoDTO.class))
         );
     }
+    @RouterOperation(beanClass = ObtenerEquipoPorIdUseCase.class, beanMethod = "apply",
+            operation = @Operation(operationId = "obtenerEquipoPorId", summary = "Obtener equipo por id", tags = {"Equipo"},
+                    parameters = {@Parameter(in = ParameterIn.PATH, name = "id", description = "Integer")},
+                    responses = {@ApiResponse(responseCode = "200", description = "OK"),
+                            @ApiResponse(responseCode = "401", description = "Unauthorized", content = { @Content(examples = { @ExampleObject(value = "")})}),
+                            @ApiResponse(responseCode = "403", description = "Forbidden", content = { @Content(examples = { @ExampleObject(value = "")})}),
+                            @ApiResponse(responseCode = "404", description = "Not found", content = { @Content(examples = { @ExampleObject(value = "")})}),
+                            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = { @Content(examples = { @ExampleObject(value = "")})}),}))
     @Bean
     public RouterFunction<ServerResponse> obtenerEquipoPorId(ObtenerEquipoPorIdUseCase useCase){
         return route(GET("/obtenerequipoid/{id}").and(accept(MediaType.APPLICATION_JSON)),
@@ -71,6 +117,14 @@ public class EquipoRouter {
         );
     }
 
+    @RouterOperation(beanClass = ObtenerEquipoPorCodigoUseCase.class, beanMethod = "apply",
+            operation = @Operation(operationId = "obtenerEquipoPorCodigo", summary = "Obtener equipo por codigo", tags = {"Equipo"},
+                    parameters = {@Parameter(in = ParameterIn.PATH, name = "codigo", description = "Integer")},
+                    responses = {@ApiResponse(responseCode = "200", description = "OK"),
+                            @ApiResponse(responseCode = "401", description = "Unauthorized", content = { @Content(examples = { @ExampleObject(value = "")})}),
+                            @ApiResponse(responseCode = "403", description = "Forbidden", content = { @Content(examples = { @ExampleObject(value = "")})}),
+                            @ApiResponse(responseCode = "404", description = "Not found", content = { @Content(examples = { @ExampleObject(value = "")})}),
+                            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = { @Content(examples = { @ExampleObject(value = "")})}),}))
     @Bean
     public RouterFunction<ServerResponse> obtenerEquipoPorCodigo(ObtenerEquipoPorCodigoUseCase useCase){
         return route(GET("/obtenerequipocodigo/{codigo}").and(accept(MediaType.APPLICATION_JSON)),
